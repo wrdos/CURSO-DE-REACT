70417577e88f88d9192a23b3d6dedc00419496c8
Williams <williamsrogerio@gmail.com>
2025-08-20 19:07:56 -0300

    Ajuste check isComplete

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, Trash2Icon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, Trash2Icon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -18,10 +18,9 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
-              task.isCompleted && "line-through"
-            }`}
+            className={`bg-slate-400 text-left w-full flex items-center text-white p-2 rounded-md `}
           >
+            {task.isCompleted && <CheckIcon />}
             {task.title}
           </button>
 
